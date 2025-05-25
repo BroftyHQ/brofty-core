59d4cdd8637caf875cf87faf7d1118a0ffa0dab6
Brofty Admin <admin@brofty.com>
2025-05-26 02:24:40 +0530

    refactor: standardize import statements and improve code formatting in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 // npm install @apollo/server @as-integrations/express5 express graphql cors
-import { ApolloServer } from '@apollo/server';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import { expressMiddleware } from '@as-integrations/express5';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
-import typeDefs from './typedefs';
-import resolvers from './resolvers';
-import { WebSocketServer } from 'ws';
-import { makeExecutableSchema } from '@graphql-tools/schema';
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@as-integrations/express5";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import typeDefs from "./typedefs";
+import resolvers from "./resolvers";
+import { WebSocketServer } from "ws";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 //@ts-ignore
-import { useServer } from 'graphql-ws/use/ws';
-import { AnonymousGraphQLContext, AuthorizedGraphQLContext } from './types/context';
-import { firebaseAuthApp } from './firebase';
+import { useServer } from "graphql-ws/use/ws";
+import {
+  AnonymousGraphQLContext,
+  AuthorizedGraphQLContext,
+} from "./types/context";
+import { firebaseAuthApp } from "./firebase";
 
 interface MyContext {
   token?: string;
@@ -27,21 +30,21 @@ const httpServer = http.createServer(app);
 const schema = makeExecutableSchema({
   typeDefs,
   resolvers,
-})
+});
 // Creating the WebSocket server
 const wsServer = new WebSocketServer({
   // This is the `httpServer` we created in a previous step.
   server: httpServer,
   // Pass a different path here if app.use
   // serves expressMiddleware at a different path
-  path: '/v1/subscriptions',
+  path: "/v1/subscriptions",
 });
 const serverCleanup = useServer({ schema }, wsServer);
 
 // Same ApolloServer initialization as before, plus the drain plugin
 // for our httpServer.
 const server = new ApolloServer<MyContext>({
-    schema,
+  schema,
   plugins: [
     // Proper shutdown for the HTTP server.
     ApolloServerPluginDrainHttpServer({ httpServer }),
@@ -80,55 +83,60 @@ const corsConfig = cors<cors.CorsRequest>({
   },
 });
 
+app.get("/rest/v1/status", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    message: "Brofty SSR server is running",
+  });
+});
 
 // Set up our Express middleware to handle CORS, body parsing,
 // and our expressMiddleware function.
 app.use(
-  '/v1',
+  "/v1",
   corsConfig,
   express.json(),
   // expressMiddleware accepts the same arguments:
   // an Apollo Server instance and optional configuration options
   expressMiddleware(server, {
     context: async ({ req }: { req: any }) => {
-    const contextIfNotAuthorized: AnonymousGraphQLContext = {
-    };
+      const contextIfNotAuthorized: AnonymousGraphQLContext = {};
 
-    if (req.body.operationName === "IntrospectionQuery") {
-      return contextIfNotAuthorized;
-    }
-
-    const authorization = req.headers.authorization;
-    if (authorization) {
-      var payload: any;
-      try {
-        payload = await firebaseAuthApp.verifyIdToken(authorization);
-      } catch (error: any) {
-        if (authorization === "brofty-srr-server") {
-          return {
-            user: {
-              email: "ssr.server@brofty.com",
-              email_verified: payload.email_verified,
-            }
-          } as AuthorizedGraphQLContext;
-        }
+      if (req.body.operationName === "IntrospectionQuery") {
         return contextIfNotAuthorized;
       }
-      return {
-        user: {
-          email: payload.email,
-          email_verified: payload.email_verified,
-        },
-      } as AuthorizedGraphQLContext;
-    } else {
-      return contextIfNotAuthorized;
-    }
-  },
-  }),
+
+      const authorization = req.headers.authorization;
+      if (authorization) {
+        var payload: any;
+        try {
+          payload = await firebaseAuthApp.verifyIdToken(authorization);
+        } catch (error: any) {
+          if (authorization === "brofty-srr-server") {
+            return {
+              user: {
+                email: "ssr.server@brofty.com",
+                email_verified: payload.email_verified,
+              },
+            } as AuthorizedGraphQLContext;
+          }
+          return contextIfNotAuthorized;
+        }
+        return {
+          user: {
+            email: payload.email,
+            email_verified: payload.email_verified,
+          },
+        } as AuthorizedGraphQLContext;
+      } else {
+        return contextIfNotAuthorized;
+      }
+    },
+  })
 );
 
 // Modified server startup
 await new Promise<void>((resolve) =>
-  httpServer.listen({ port: process.env.PORT || 4000 }, resolve),
+  httpServer.listen({ port: process.env.PORT || 4000 }, resolve)
 );
-console.log(`🚀 Server ready at http://localhost:4000/`);
+console.log(`🚀 Server ready`);
