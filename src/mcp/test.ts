977da08632e5ec10a78c810e4dd0e62a600e9294
Brofty Admin <admin@brofty.com>
2025-06-17 23:59:18 +0530

    feat: update getMcpClient to inherit stderr for improved error visibility in console

--- src/mcp/test.ts
+++ src/mcp/test.ts
@@ -1,8 +1,27 @@
-import add_availble_mcp_tools from "./add_availble_mcp_tools.js";
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+import logger from "../common/logger.js";
 
 async function main() {
-  add_availble_mcp_tools("notion-mcp");
+  const transport = new StdioClientTransport({
+    command: "npx", // Use the command from servers
+    args: ["-y", "@tokenizin/mcp-npx-fetch"], // Use the args from servers
+    env: {}, // Use the env from servers, default to empty object
+  });
+  const client = new Client({
+    name: "brofty", // A name for your client
+    version: "1.0.0", // A version for your client
+  });
+    try {
+      await client.connect(transport); // Set the transport for the client
+    } catch (error) {
+      logger.error(
+        `Failed to connect MCP client : ${error instanceof Error ? error.message : String(error)}`
+      );
+    }
 
+    console.log(await client.listTools());
+    await client.close();
 }
 
 // Run the main function
